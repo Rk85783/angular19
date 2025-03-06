2d9fd0a687c76d90f8d433e17977ee2c67c2854a
Rohit Kumar Mahor (rk85783) <rk85783@gmail.com>
2025-03-06 12:21:31 +0530

    08_custom_component

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-profile',
+  // template: '<h2>Profile Component</h2>',
+  templateUrl: './profile.component.html',
+  // styles: 'h2{background: red}'
+  styleUrl: './profile.component.css'
+  // styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent {
+
+}
