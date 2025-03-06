7933c1d15439281f5fac6f46fc701464987958a5
Rohit Kumar Mahor (rk85783) <rk85783@gmail.com>
2025-03-06 13:46:48 +0530

    12 click, input, mouse, foucs, and blur events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'myApp';
+  handleEvent(event: Event) {
+    console.log("function called", event.type);
+    // console.log("function called", (event.target as HTMLInputElement).name);
+    // console.log("function called", (event.target as HTMLInputElement).classList);
+    console.log("function called", (event.target as HTMLInputElement).value);
+  }
 }
