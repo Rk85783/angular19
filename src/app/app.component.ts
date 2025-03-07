81cef813f46c1dd1b4ac55ef5f13ab38c3e1795a
Rohit Kumar Mahor (rk85783) <rk85783@gmail.com>
2025-03-07 11:20:15 +0530

    18 @For Loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,18 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'myApp';
+
+  users = ["Rohit", "Neeraj", "Lokesh", "Pradeep", "Rekha", "Nancy"];
+  students = [
+    { id: 1, name: "Rohit", age: 30 },
+    { id: 2, name: "Neeraj", age: 28 },
+    { id: 3, name: "Lokesh", age: 22 },
+    { id: 4, name: "Pradeep", age: 35 },
+    { id: 5, name: "Rekha", age: 28 },
+    { id: 6, name: "Nancy", age: 28 }
+  ];
+
+  getName(name: string) {
+    alert(name);
+  }
 }
