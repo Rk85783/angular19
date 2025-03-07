547e6516d1e4ceba28f19b96d1421e19dcc7ec33
Rohit Kumar Mahor (rk85783) <rk85783@gmail.com>
2025-03-07 11:41:08 +0530

    19 Signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,26 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'myApp';
+
+  x = 20;
+  count = signal(0);
+
+  constructor() {
+    effect(() => {
+      console.log(this.count());
+    })
+  }
+
+  updateValue() {
+    this.count.set(this.count() + 1);
+    // this.x = this.x + 1;
+  }
+
+  updateValue2(val: string) {
+    if (val == 'inc') {
+      this.count.set(this.count() + 1);
+    } else if (val == 'dec') {
+      this.count.set(this.count() - 1);
+    }
+  }
 }
